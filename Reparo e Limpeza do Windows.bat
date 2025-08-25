1b9dce7a79a3e42259ad9507183525dccfcba37d
Eric Luan <42724268+eriicluanz@users.noreply.github.com>
2025-08-25 15:01:55 -0300

    Reparo e Limpeza do Windows.bat

--- Reparo e Limpeza do Windows.bat
+++ Reparo e Limpeza do Windows.bat
@@ -15,14 +15,14 @@ echo Iniciando reparos... > "%LOG_FILE%"
 
 :: 1. Verificando arquivos do sistema
 echo.
-echo [1/5] Executando SFC...
+echo Executando SFC...
 echo ---- SFC ---- >> "%LOG_FILE%"
 sfc /scannow >> "%LOG_FILE%" 2>&1
 echo SFC concluído!
 echo.
 
 :: 2. Verificando imagem do Windows com DISM
-echo [2/5] Executando DISM...
+echo Executando DISM...
 echo ---- DISM ---- >> "%LOG_FILE%"
 DISM /Online /Cleanup-Image /CheckHealth >> "%LOG_FILE%" 2>&1
 DISM /Online /Cleanup-Image /ScanHealth >> "%LOG_FILE%" 2>&1
@@ -31,7 +31,7 @@ echo DISM concluído!
 echo.
 
 :: 3. Limpando arquivos temporarios
-echo [3/5] Limpando arquivos temporários...
+echo Limpando arquivos temporários...
 echo ---- Limpeza de Temp ---- >> "%LOG_FILE%"
 del /s /q %temp%\* >> "%LOG_FILE%" 2>&1
 del /s /q C:\Windows\Temp\* >> "%LOG_FILE%" 2>&1
@@ -39,7 +39,7 @@ echo Limpeza de arquivos temporários concluída!
 echo.
 
 :: 4. Reiniciando servicos criticos
-echo [4/5] Reiniciando serviços críticos...
+echo Reiniciando serviços críticos...
 echo ---- Reinicio de Serviços ---- >> "%LOG_FILE%"
 net stop wuauserv >> "%LOG_FILE%" 2>&1
 net start wuauserv >> "%LOG_FILE%" 2>&1
@@ -49,7 +49,8 @@ echo Serviços reiniciados!
 echo.
 
 :: 5. Criando log final
-echo [5/5] Reparos concluidos!
+echo Reparos concluidos!
 echo Todos os reparos foram concluidos. Log disponivel em: "%LOG_FILE%"
 echo ==========================================
+
 pause
